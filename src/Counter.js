c8e547b7bcb7de072b2fc3f9e1c4af2b5e9e1a89
강태민 <103995917+PunchFather@users.noreply.github.com>
2022-05-12 07:19:35 +0900

    Update Counter.js

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,14 @@
 import { useState } from "react";
 import OddEvent from "./OddEvent";
 
-const Counter = ({ initiatevalue }) => {
+const Counter = ({ initiatevalue }) => { // APP.JS에서 Prop으로 받은 값이다. number객체의 값인 initiatevalue 받을때 {} 안에 넣어서 호출한다. 
   const [count, setCount] = useState(initiatevalue);
 
-  const IncreasePlus = () => {
+  const IncreasePlus = () => { // 1씩 증가하는 함수 
     setCount(count + 1);
   };
 
-  const decreaseminus = () => {
+  const decreaseminus = () => { // 1씩 감소 함수
     setCount(count - 1);
   };
 
@@ -17,12 +17,13 @@ const Counter = ({ initiatevalue }) => {
       <h2>{count}</h2>
       <button onClick={IncreasePlus}>+</button>
       <button onClick={decreaseminus}>-</button>
-      <OddEvent count={count} />
+      <OddEvent count={count} /> {/*동적인 state를 prop한다.*/}
+
     </div>
   );
 };
 
-Counter.defaultProps = {
+Counter.defaultProps = { // APP.JS에서 initiatevalue이 값을 전달 받지 못했을 때 기본값을 1로 설정한다.
   initiatevalue: 1,
 };
 
